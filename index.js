122cf5a9f42365a6dd79581e49ecaad7d7a87d7e
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2022-12-14 23:53:13 +0530

    signup page completed

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger=document.querySelector(".hamburger");
+const navMenu=document.querySelector(".nav-menu");
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
