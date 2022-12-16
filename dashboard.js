f8ed8d7ba7c7c50dcb20b00db05f6c0d8d86e13b
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2022-12-16 22:44:07 +0530

    dashboard complete

--- dashboard.js
+++ dashboard.js
@@ -7,17 +7,12 @@ let sidebar = document.querySelector(".sidebar");
     menuBtnChange();//calling the function(optional)
   });
 
-  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-    
-  });
-
   // following are the code to change sidebar button(optional)
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-   }
-  }
+
+ let listenbtn=document.querySelector(".listen")
+ listenbtn.addEventListener("click",()=>{
+  window.location.assign('https://cronometer.podbean.com/e/episode-8-the-mastering-diabetes-episode/?utm_source=email&utm_campaign=email_2022-11-30_diabetes-wrap');
+ })
+
+ 
+ 
